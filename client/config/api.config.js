5eb54241f52835c94e7c227dbb7fb2962864b5f0
congcong888 <tiancong0808@163.com>
2017-10-17 17:03:50 +0800

    修改配置文件

--- client/config/api.config.js
+++ client/config/api.config.js
@@ -1,5 +1,5 @@
 const isProdMode = Object.is(process.env.NODE_ENV, 'production')
 
 module.exports = {
-    baseUrl: isProdMode ? 'http://is.tcongcong.site/api/' : 'api/'
+    baseUrl: isProdMode ? 'http://mall.tcongcong.site/api/' : 'api/'
 }
