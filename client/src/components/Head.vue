eb01f276d2312ce8103d7da85fe0eb4efdb5282e
congcong888 <tiancong0808@163.com>
2017-10-20 16:32:44 +0800

    修改关闭按钮

--- client/src/components/Head.vue
+++ client/src/components/Head.vue
@@ -38,7 +38,7 @@
             <div class="md-modal-inner">
                 <div class="md-top">
                     <div class="md-title">login in</div>
-                    <button class="md-close" @click="mdShow = false">Close</button>
+                    <button class="md-close">Close</button>
                 </div>
                 <div class="md-content">
                     <div class="confirm-tips">
@@ -76,8 +76,7 @@ export default {
             userName: '',
             userPwd: '',
             nickName: '',
-            loginModalFlag: false,
-            mdShow:false
+            loginModalFlag: false
         }
     },
     mounted: function() {
