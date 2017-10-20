721a2000b08b5aaeabac10ac80211f750d0ee397
congcong888 <tiancong0808@163.com>
2017-10-20 16:39:54 +0800

    修改关闭按钮

--- client/src/components/Head.vue
+++ client/src/components/Head.vue
@@ -38,7 +38,7 @@
             <div class="md-modal-inner">
                 <div class="md-top">
                     <div class="md-title">login in</div>
-                    <button class="md-close" :class="{'md-show' : loginModalFlag}">Close</button>
+                    <button class="md-close" @click="loginModalFlag = false">Close</button>
                 </div>
                 <div class="md-content">
                     <div class="confirm-tips">
